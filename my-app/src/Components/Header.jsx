9c8d188ff12cea057767992db98c40bfdc7d078d
Nilay <nilay.service@gmail.com>
2023-03-14 05:44:40 +0530

    components commit-1

---
+++ my-app/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <div><h1>Header</h1>
+            <p>This is amazing tutorial</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
